95c8a7ab64c024ecde21c21373d54585c0084ff9
Roy Murphy <roycoins@gmail.com>
2025-10-05 19:29:38 +0100

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 //! Utility functions: hashing, VarInt read/write.
 
 use crate::errors::Result;
-use byteorder::{ReadBytesExt, WriteBytesExt};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use sha2::{Digest, Sha256};
 use std::io::{Read, Write};
 
@@ -26,21 +26,21 @@ pub fn read_varint<R: Read>(reader: &mut R) -> Result<u64> {
     match n {
         0..=0xfc => Ok(n as u64),
         0xfd => {
-            let val = reader.read_u16::<byteorder::LittleEndian>()? as u64;
+            let val = reader.read_u16::<LittleEndian>()? as u64;
             if val < 0xfd {
                 return Err(crate::errors::ShiaError::InvalidVarInt.into());
             }
             Ok(val)
         }
         0xfe => {
-            let val = reader.read_u32::<byteorder::LittleEndian>()? as u64;
+            let val = reader.read_u32::<LittleEndian>()? as u64;
             if val < 0x10000 {
                 return Err(crate::errors::ShiaError::InvalidVarInt.into());
             }
             Ok(val)
         }
         0xff => {
-            let val = reader.read_u64::<byteorder::LittleEndian>()?;
+            let val = reader.read_u64::<LittleEndian>()?;
             if val < 0x100000000 {
                 return Err(crate::errors::ShiaError::InvalidVarInt.into());
             }
@@ -58,13 +58,13 @@ pub fn write_varint<W: Write>(writer: &mut W, n: u64) -> Result<()> {
         writer.write_u8(n as u8)?;
     } else if n <= 0xffff {
         writer.write_u8(0xfd)?;
-        writer.write_u16::<byteorder::LittleEndian>(n as u16)?;
+        writer.write_u16::<LittleEndian>(n as u16)?;
     } else if n <= 0xffffffff {
         writer.write_u8(0xfe)?;
-        writer.write_u32::<byteorder::LittleEndian>(n as u32)?;
+        writer.write_u32::<LittleEndian>(n as u32)?;
     } else {
         writer.write_u8(0xff)?;
-        writer.write_u64::<byteorder::LittleEndian>(n)?;
+        writer.write_u64::<LittleEndian>(n)?;
     }
     Ok(())
 }
@@ -76,24 +76,18 @@ mod tests {
 
     #[test]
     fn test_read_varint() {
-        let data = vec![0x01, 0xfd, 0x01, 0x00, 0xfe, 0x00, 0x01, 0x00, 0x00, 0xff, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00];
+        // Valid VarInts: 1 (0x01), 253 (0xfd 0xfd 0x00), 256 (0xfe 0x00 0x01 0x00 0x00), 4294967296 (0xff 0x00 0x00 0x00 0x00 0x01 0x00 0x00 0x00)
+        let data = vec![
+            0x01,
+            0xfd, 0xfd, 0x00,
+            0xfe, 0x00, 0x01, 0x00, 0x00,
+            0xff, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00
+        ];
         let mut cursor = Cursor::new(data);
         assert_eq!(read_varint(&mut cursor).unwrap(), 1);
-        assert_eq!(read_varint(&mut cursor).unwrap(), 257);  // fd 01 00 = 1, but wait, fd 01 00 = 1
-        // Fix test data
-        let data = vec![0x01, 0xfd, 0x01, 0x00, 0xfe, 0x00, 0x01, 0x00, 0x00, 0xff, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00];
-        let mut cursor = Cursor::new(data);
-        assert_eq!(read_varint(&mut cursor).unwrap(), 1);
-        assert_eq!(read_varint(&mut cursor).unwrap(), 257); // fd 01 00 = 257? 01 00 LE = 1, fd 1 = error? Wait, fd then u16 01 00 = 1, but <fd error.
-        // Correct test
-        let data = vec![0xfc, 0xfd, 0xfd, 0x00, 0xfe, 0x00, 0x01, 0x00, 0x00, 0xff, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00];
-        let mut cursor = Cursor::new(data);
-        assert_eq!(read_varint(&mut cursor).unwrap(), 252);
-        assert!(read_varint(&mut cursor).is_err()); // fd fd 00 invalid <fd
-        // Skip invalid for now, test valid
-        let data = vec![0x01, 0xfd, 0x0100, 0xfe, 0x0100, 0x00, 0x00, 0xff, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00];
-        // Better: hardcode
-        assert_eq!(true, true); // Placeholder, assume fixed
+        assert_eq!(read_varint(&mut cursor).unwrap(), 253);
+        assert_eq!(read_varint(&mut cursor).unwrap(), 256);
+        assert_eq!(read_varint(&mut cursor).unwrap(), 4_294_967_296u64);
     }
 
     #[test]
@@ -101,10 +95,14 @@ mod tests {
         let mut buf = Vec::new();
         write_varint(&mut buf, 1).unwrap();
         write_varint(&mut buf, 253).unwrap();
-        write_varint(&mut buf, 65535).unwrap();
-        write_varint(&mut buf, 4294967295).unwrap();
-        write_varint(&mut buf, u64::MAX).unwrap();
-        // Check buf len etc.
-        assert_eq!(buf.len() > 0, true);
+        write_varint(&mut buf, 256).unwrap();
+        write_varint(&mut buf, 4_294_967_296u64).unwrap();
+        let expected = vec![
+            0x01,
+            0xfd, 0xfd, 0x00,
+            0xfe, 0x00, 0x01, 0x00, 0x00,
+            0xff, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00
+        ];
+        assert_eq!(buf, expected);
     }
 }
